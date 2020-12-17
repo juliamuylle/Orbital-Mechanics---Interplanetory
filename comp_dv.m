216e37228320068f620ed8f8ab25527c25a6cba1
juliamuylle <75630206+juliamuylle@users.noreply.github.com>
2020-12-17 13:35:54 +0100

    Update comp_dv.m

--- comp_dv.m
+++ comp_dv.m
@@ -1,25 +1,21 @@
 function [delta_v,VI,VF] = comp_dv(r1,v1,r2,v2,dep,ar,ksun)
 
 
-
-
 orbitType = 0; %0 if prog; 1 if retrog
 Nrev = 0; 
 Ncase = 0;
 optionsLMR = 0;
 
-for i = 1:length(r1(1,:))
-    for j = 1:length(r2(1,:))
-        TOF = (ar(j)-dep(i))*24*3600;
-        if TOF > 0
-            [A,P,E,ERROR,VI,VF,TPAR,THETA] = lambertMR(r1(:,i),r2(:,j),TOF,ksun,orbitType,Nrev,Ncase,optionsLMR);
-            delta_v1 =VI'- v1(:,i);
-            delta_v2 = v2(:,j)-VF';
-            delta_v(i,j) = norm(delta_v1)+norm(delta_v2);
-        else
-            delta_v(i,j) = NaN;
-        end
-    end
+TOF = (ar-dep)*24*3600;
+if TOF > 0
+    [A,P,E,ERROR,VI,VF,TPAR,THETA] = lambertMR(r1,r2,TOF,ksun,orbitType,Nrev,Ncase,optionsLMR);
+    delta_v1 = VI'- v1;
+    delta_v2 = v2-VF';
+    delta_v = norm(delta_v1)+norm(delta_v2);
+else
+    delta_v = NaN;
+    VI = NaN;
+    VF = NaN;
 end
         
     
