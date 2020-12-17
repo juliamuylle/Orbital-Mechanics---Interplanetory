baaecdd24f94dce2f9fd14eac60028d481778cd9
juliamuylle <75630206+juliamuylle@users.noreply.github.com>
2020-12-17 13:37:48 +0100

    Update flybyPow.m

--- flybyPow.m
+++ flybyPow.m
@@ -1,40 +1,39 @@
-function [deltav_perig,rp,delta,arcs] = flybyPow(vinfM,vinfP,mu_E,rp0)
+function [deltav_perig,rp] = flybyPow(vinfM,vinfP,mu_E,hatm,rp0)
 %[DV_ga,DV_fb,rp,steps,delta,arcs]
 
 delta_true = acos(dot(vinfM,vinfP)/(norm(vinfM)*norm(vinfP)));
+
 deta_deg = rad2deg(delta_true);
 
-eM = @(rp) 1+(rp*(norm(vinfM))^2)/mu_E;
-deltaM = @(rp) 2*asin(1/eM(rp));
+eM = @(rp) 1+((rp*(norm(vinfM))^2)/mu_E);
+deltaM = @(rp) 2*asin(1./eM(rp));
 
-eP = @(rp) 1+(rp*(norm(vinfP))^2)/mu_E;
-deltaP = @(rp) 2*asin(1/eP(rp));
+eP = @(rp) 1+((rp*(norm(vinfP))^2)/mu_E);
+deltaP = @(rp) 2*asin(1./eP(rp));
 
 delta = @(rp) deltaM(rp)/2+deltaP(rp)/2;
 
 FUN = @(rp) delta(rp)-delta_true;
 
 
-rp = fzero(FUN,rp0);
-%hatm = 
+rp = fsolve(FUN,rp0);
 
-% if rp < Re + hatm
-%     error('rp is too small');
-% end
+if rp < 6371+hatm
+     rp = NaN;
+end
 
-eM = eM(rp);
-eP = eP(rp);
+eM_val = eM(rp);
+eP_val = eP(rp);
 
-hM = sqrt(mu_E*rp*(1+eM));
-vperig_M = mu_E/hM*(1+eM);
+hM = sqrt(mu_E*rp*(1+eM_val));
+vperig_M = mu_E/hM*(1+eM_val);
 
-hP = sqrt(mu_E*norm(rp)*(1+eP));
-vperig_P = mu_E/hP*(1+eP);
+hP = sqrt(mu_E*norm(rp)*(1+eP_val));
+vperig_P = mu_E/hP*(1+eP_val);
 
 deltav_perig = abs(vperig_M-vperig_P);
 
-delta = delta_true;
 
-arc1 = [eM,vperig_M];
-arc2 = [eP,vperig_P];
+arc1 = [eM_val,vperig_M];
+arc2 = [eP_val,vperig_P];
 arcs = [arc1;arc2];
